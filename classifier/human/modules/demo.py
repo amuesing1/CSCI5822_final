abaa401b2ccef0019a0ef92575d828e554072bfa
Jeremy <amuesing1@gmail.com>
2018-04-30 18:19:42 -0600

    added hmm, updates, gibbs sampling

--- classifier/human/modules/demo.py
+++ classifier/human/modules/demo.py
@@ -3,28 +3,38 @@ from PyQt4.QtGui import *
 import sys
 import numpy as np
 import scipy.stats
+from scipy import spatial
 import random
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 sys.path.append('../../../scenario_simulator/modules')
+sys.path.append('../../HMM/modules')
 from regions import Region
 from DynamicsProfiles import *
+from hmm_classification import HMM_Classification
 
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
 
 class Window(QtGui.QWidget):
-    def __init__(self,total_data,prior):
+    def __init__(self,total_data,intensity,prior):
         super(Window,self).__init__()
         self.frame=0
         self.all_data=total_data
+        self.intensity=intensity
         self.table=prior
-        self.prev_obs=0
+        self.prev_obs=range(15)
+        self.total_obs=0
         self.matplotlibWidget=MatplotlibWidget(self)
         self.matplotlibWidget2=MatplotlibWidget2(self)
+        self.matplotlibWidget3=MatplotlibWidget3(self)
+        self.matplotlibWidget4=MatplotlibWidget4(self)
         self.matplotlibWidget.move(25,25)
         self.matplotlibWidget2.move(550,300)
+        self.matplotlibWidget3.move(25,300)
+        self.matplotlibWidget4.move(500,25)
+        self.updateProbs()
         means=scipy.stats.dirichlet.mean(alpha=np.reshape(self.table,(1,self.table.shape[0]*self.table.shape[1]*self.table.shape[2]))[0])
         new_means=np.reshape(np.array(means),(5,15,15))
         self.matplotlibWidget2.axis2.clear()
@@ -49,6 +59,8 @@ class Window(QtGui.QWidget):
 
         self.updateTimer.start()
         self.updateTimer.timeout.connect(self.updateGraph)
+        self.updateTimer.timeout.connect(self.updateIntensity)
+        self.updateTimer.timeout.connect(lambda: self.updateProbs(updateType='machine'))
 
         self.setGeometry(350,350,1250,900)
 
@@ -56,6 +68,7 @@ class Window(QtGui.QWidget):
 
         self.updatebutton=QtGui.QPushButton("Update",self)
         self.updatebutton.move(775,200)
+        self.updatebutton.clicked.connect(lambda: self.updateProbs(updateType='human'))
         self.updatebutton.clicked.connect(self.updateDir)
         self.layoutStratiform = QtGui.QHBoxLayout()
         self.widgetStratiform = QtGui.QWidget(self)
@@ -68,14 +81,13 @@ class Window(QtGui.QWidget):
         self.stratiformGroup = QtGui.QButtonGroup(self.widgetStratiform)
         self.rStratiformYes = QtGui.QRadioButton('Yes')
         self.stratiformGroup.addButton(self.rStratiformYes)
-        self.rStratiformNull = QtGui.QRadioButton('Null')
-        self.rStratiformNull.setChecked(True)
+        self.rStratiformNull = QtGui.QRadioButton('IDK')
         self.stratiformGroup.addButton(self.rStratiformNull)
         self.rStratiformNo = QtGui.QRadioButton('No')
         self.stratiformGroup.addButton(self.rStratiformNo)
         self.layoutStratiform.addWidget(self.rStratiformYes)
-        self.layoutStratiform.addWidget(self.rStratiformNull)
         self.layoutStratiform.addWidget(self.rStratiformNo)
+        self.layoutStratiform.addWidget(self.rStratiformNull)
 
         self.layoutCirriform = QtGui.QHBoxLayout()
         self.widgetCirriform = QtGui.QWidget(self)
@@ -88,14 +100,13 @@ class Window(QtGui.QWidget):
         self.CirriformGroup = QtGui.QButtonGroup(self.widgetCirriform)
         self.rCirriformYes = QtGui.QRadioButton('Yes')
         self.CirriformGroup.addButton(self.rCirriformYes)
-        self.rCirriformNull = QtGui.QRadioButton('Null')
-        self.rCirriformNull.setChecked(True)
+        self.rCirriformNull = QtGui.QRadioButton('IDK')
         self.CirriformGroup.addButton(self.rCirriformNull)
         self.rCirriformNo = QtGui.QRadioButton('No')
         self.CirriformGroup.addButton(self.rCirriformNo)
         self.layoutCirriform.addWidget(self.rCirriformYes)
-        self.layoutCirriform.addWidget(self.rCirriformNull)
         self.layoutCirriform.addWidget(self.rCirriformNo)
+        self.layoutCirriform.addWidget(self.rCirriformNull)
 
         self.layoutStratoCumuliform = QtGui.QHBoxLayout()
         self.widgetStratoCumuliform = QtGui.QWidget(self)
@@ -108,14 +119,13 @@ class Window(QtGui.QWidget):
         self.StratoCumuliformGroup = QtGui.QButtonGroup(self.widgetStratoCumuliform)
         self.rStratoCumuliformYes = QtGui.QRadioButton('Yes')
         self.StratoCumuliformGroup.addButton(self.rStratoCumuliformYes)
-        self.rStratoCumuliformNull = QtGui.QRadioButton('Null')
-        self.rStratoCumuliformNull.setChecked(True)
+        self.rStratoCumuliformNull = QtGui.QRadioButton('IDK')
         self.StratoCumuliformGroup.addButton(self.rStratoCumuliformNull)
         self.rStratoCumuliformNo = QtGui.QRadioButton('No')
         self.StratoCumuliformGroup.addButton(self.rStratoCumuliformNo)
         self.layoutStratoCumuliform.addWidget(self.rStratoCumuliformYes)
-        self.layoutStratoCumuliform.addWidget(self.rStratoCumuliformNull)
         self.layoutStratoCumuliform.addWidget(self.rStratoCumuliformNo)
+        self.layoutStratoCumuliform.addWidget(self.rStratoCumuliformNull)
 
 
         self.layoutCumuliform = QtGui.QHBoxLayout()
@@ -129,14 +139,13 @@ class Window(QtGui.QWidget):
         self.CumuliformGroup = QtGui.QButtonGroup(self.widgetCumuliform)
         self.rCumuliformYes = QtGui.QRadioButton('Yes')
         self.CumuliformGroup.addButton(self.rCumuliformYes)
-        self.rCumuliformNull = QtGui.QRadioButton('Null')
-        self.rCumuliformNull.setChecked(True)
+        self.rCumuliformNull = QtGui.QRadioButton('IDK')
         self.CumuliformGroup.addButton(self.rCumuliformNull)
         self.rCumuliformNo = QtGui.QRadioButton('No')
         self.CumuliformGroup.addButton(self.rCumuliformNo)
         self.layoutCumuliform.addWidget(self.rCumuliformYes)
-        self.layoutCumuliform.addWidget(self.rCumuliformNull)
         self.layoutCumuliform.addWidget(self.rCumuliformNo)
+        self.layoutCumuliform.addWidget(self.rCumuliformNull)
 
 
         self.layoutCumulonibiform = QtGui.QHBoxLayout()
@@ -150,14 +159,13 @@ class Window(QtGui.QWidget):
         self.CumulonibiformGroup = QtGui.QButtonGroup(self.widgetCumulonibiform)
         self.rCumulonibiformYes = QtGui.QRadioButton('Yes')
         self.CumulonibiformGroup.addButton(self.rCumulonibiformYes)
-        self.rCumulonibiformNull = QtGui.QRadioButton('Null')
-        self.rCumulonibiformNull.setChecked(True)
+        self.rCumulonibiformNull = QtGui.QRadioButton('IDK')
         self.CumulonibiformGroup.addButton(self.rCumulonibiformNull)
         self.rCumulonibiformNo = QtGui.QRadioButton('No')
         self.CumulonibiformGroup.addButton(self.rCumulonibiformNo)
         self.layoutCumulonibiform.addWidget(self.rCumulonibiformYes)
-        self.layoutCumulonibiform.addWidget(self.rCumulonibiformNull)
         self.layoutCumulonibiform.addWidget(self.rCumulonibiformNo)
+        self.layoutCumulonibiform.addWidget(self.rCumulonibiformNull)
 
 
 
@@ -169,59 +177,14 @@ class Window(QtGui.QWidget):
         self.matplotlibWidget.canvas.draw()
         self.frame+=1
 
+    def updateIntensity(self):
+        self.matplotlibWidget3.axis3.clear()
+        self.matplotlibWidget3.axis3.plot(range(self.frame),self.intensity[:self.frame])
+        self.matplotlibWidget3.axis3.set_ylim(0,max(self.intensity)+1)
+        self.matplotlibWidget3.canvas3.draw()
+
     def updateDir(self):
-        obs=[]
-        if(self.rStratiformYes.isChecked()):
-            obs.append(0)
-            self.rStratiformNull.setChecked(True)
-        elif(self.rStratiformNull.isChecked()):
-            obs.append(1)
-            self.rStratiformNull.setChecked(True) 
-        elif(self.rStratiformNo.isChecked()):
-            obs.append(2)
-            self.rStratiformNull.setChecked(True)
-
-        if(self.rCirriformYes.isChecked()):
-            obs.append(3)
-            self.rCirriformNull.setChecked(True)
-        elif(self.rCirriformNull.isChecked()):
-            obs.append(4)
-            self.rCirriformNull.setChecked(True) 
-        elif(self.rCirriformNo.isChecked()):
-            obs.append(5)
-            self.rCirriformNull.setChecked(True)
-
-        if(self.rStratoCumuliformYes.isChecked()):
-            obs.append(6)
-            self.rStratoCumuliformNull.setChecked(True)
-        elif(self.rStratoCumuliformNull.isChecked()):
-            obs.append(7)
-            self.rStratoCumuliformNull.setChecked(True) 
-        elif(self.rStratoCumuliformNo.isChecked()):
-            obs.append(8)
-            self.rStratoCumuliformNull.setChecked(True)
-
-        if(self.rCumuliformYes.isChecked()):
-            obs.append(9)
-            self.rCumuliformNull.setChecked(True)
-        elif(self.rCumuliformNull.isChecked()):
-            obs.append(10)
-            self.rCumuliformNull.setChecked(True) 
-        elif(self.rCumuliformNo.isChecked()):
-            obs.append(11)
-            self.rCumuliformNull.setChecked(True)
-
-        if(self.rCumulonibiformYes.isChecked()):
-            obs.append(12)
-            self.rCumulonibiformNull.setChecked(True)
-        elif(self.rCumulonibiformNull.isChecked()):
-            obs.append(13)
-            self.rCumulonibiformNull.setChecked(True) 
-        elif(self.rCumulonibiformNo.isChecked()):
-            obs.append(14)
-            self.rCumulonibiformNull.setChecked(True)
-
-        self.table[0,self.prev_obs,obs]+=.01
+        #  self.table[0,self.prev_obs,obs]+=.01
         means=scipy.stats.dirichlet.mean(alpha=np.reshape(self.table,(1,self.table.shape[0]*self.table.shape[1]*self.table.shape[2]))[0])
         new_means=np.reshape(np.array(means),(5,15,15))
         self.matplotlibWidget2.axis2.clear()
@@ -230,7 +193,7 @@ class Window(QtGui.QWidget):
         _y=np.arange(15)
         _xx,_yy=np.meshgrid(_x,_y)
         x,y=_xx.ravel(),_yy.ravel()
-        top=new_means[0,:,:]
+        top=new_means[2,:,:]
         top=np.reshape(top,(1,225))[0]
         bottom=np.zeros_like(top)
         width=depth=1
@@ -242,8 +205,131 @@ class Window(QtGui.QWidget):
         #  ax1.bar3d(x,y,bottom,width,depth,top,shade=True)
         #  plt.show()
 
-        self.prev_obs=random.choice(obs)
+        #  self.prev_obs=random.choice(obs)
 
+    def updateProbs(self,updateType=None):
+        modelFileName = '../../HMM/data/histModels_final.npy'
+        models = np.load(modelFileName).item()
+        hmm=HMM_Classification()
+        names = ['Cumuliform0','Cumuliform1','Cumuliform2','Cumuliform3','Cumuliform4']
+        if not updateType:
+            self.alphas={}
+            self.probs={}
+            for i in names:
+                self.alphas[i]=[-1,-1]
+                #  self.probs[i]=.2
+                self.probs[i]=np.random.uniform()
+            for i in names:
+                self.probs[i]/=sum(self.probs.values())
+        elif updateType=='machine':
+            data=self.intensity[self.frame]
+            #forward algorithm
+            for i in names:
+                self.alphas[i]=hmm.continueForward(data,models[i],self.alphas[i])
+                self.probs[i]=self.probs[i]*sum(self.alphas[i])
+            #noramlize
+            suma=sum(self.probs.values())
+            for i in names:
+                self.probs[i]/=suma
+        elif updateType=='human':
+            obs=[]
+            if(self.rStratiformYes.isChecked()):
+                obs.append(0)
+            elif(self.rStratiformNull.isChecked()):
+                obs.append(1)
+            elif(self.rStratiformNo.isChecked()):
+                obs.append(2)
+
+            if(self.rCirriformYes.isChecked()):
+                obs.append(3)
+            elif(self.rCirriformNull.isChecked()):
+                obs.append(4)
+            elif(self.rCirriformNo.isChecked()):
+                obs.append(5)
+
+            if(self.rStratoCumuliformYes.isChecked()):
+                obs.append(6)
+            elif(self.rStratoCumuliformNull.isChecked()):
+                obs.append(7)
+            elif(self.rStratoCumuliformNo.isChecked()):
+                obs.append(8)
+
+            if(self.rCumuliformYes.isChecked()):
+                obs.append(9)
+            elif(self.rCumuliformNull.isChecked()):
+                obs.append(10)
+            elif(self.rCumuliformNo.isChecked()):
+                obs.append(11)
+
+            if(self.rCumulonibiformYes.isChecked()):
+                obs.append(12)
+            elif(self.rCumulonibiformNull.isChecked()):
+                obs.append(13)
+            elif(self.rCumulonibiformNo.isChecked()):
+                obs.append(14)
+            
+            prev_probs=np.ones(5)
+            while spatial.distance.cosine(prev_probs,self.probs.values())!=0.0:
+            #  for k in range(5):
+                print spatial.distance.cosine(prev_probs,self.probs.values())
+                prev_probs=self.probs.values()
+                means=scipy.stats.dirichlet.mean(alpha=np.reshape(self.table,(1,self.table.shape[0]*self.table.shape[1]*self.table.shape[2]))[0])
+                new_means=np.reshape(np.array(means),(5,15,15))
+                for i in names:
+                    for prev_value in self.prev_obs:
+                        for value in obs:
+                            likelihood=new_means[names.index(i),prev_value,value]/np.sum(new_means[names.index(i),:,:])
+                            self.probs[i]*=likelihood
+                #normalize
+                suma=sum(self.probs.values())
+                for i in names:
+                    self.probs[i]/=suma
+
+                if self.total_obs>1:
+                    for prev_value in self.prev_obs:
+                        for value in obs:
+                            # this is used for redistribution
+                            sum1=sum(self.table[:,prev_value,value])
+                            for i in names:
+                                self.table[names.index(i),prev_value,value]*=self.probs[i]*(np.sum(new_means[:,prev_value,value])/np.sum(new_means))
+                            # this on is used for regular normalization
+                            sum2=sum(self.table[:,prev_value,value])
+                            for i in names:
+                                self.table[names.index(i),prev_value,value]/=sum2
+                            for i in names:
+                                self.table[names.index(i),prev_value,value]*=sum1
+                    self.table+=0.0001
+                                #  self.table[names.index(i),prev_value,value]=self.table[names.index(i),prev_value,value]*sum1+0.2*self.table[names.index(i),prev_value,value]+0.01
+
+            self.prev_obs=obs
+            self.total_obs+=1
+
+        self.matplotlibWidget4.axis4.clear()
+        self.matplotlibWidget4.axis4.bar(range(5),self.probs.values())
+        self.matplotlibWidget4.axis4.set_ylim(0,1)
+        self.matplotlibWidget4.canvas4.draw()
+
+# display probs
+class MatplotlibWidget4(QtGui.QWidget):
+    def __init__(self,parent=None):
+        super(MatplotlibWidget4,self).__init__(parent)
+        self.figure4=Figure()
+        self.canvas4=FigureCanvasQTAgg(self.figure4)
+        self.axis4=self.figure4.add_subplot(111)
+        self.layoutVertical4=QtGui.QVBoxLayout(self)
+        self.layoutVertical4.addWidget(self.canvas4)
+
+# display intensity
+class MatplotlibWidget3(QtGui.QWidget):
+    def __init__(self,parent=None):
+        super(MatplotlibWidget3,self).__init__(parent)
+        self.figure3=Figure()
+        self.canvas3=FigureCanvasQTAgg(self.figure3)
+        self.axis3=self.figure3.add_subplot(111)
+        self.layoutVertical3=QtGui.QVBoxLayout(self)
+        self.layoutVertical3.addWidget(self.canvas3)
+
+# display alphas
 class MatplotlibWidget2(QtGui.QWidget):
     def __init__(self,parent=None):
         super(MatplotlibWidget2,self).__init__(parent)
@@ -253,6 +339,7 @@ class MatplotlibWidget2(QtGui.QWidget):
         self.layoutVertical2=QtGui.QVBoxLayout(self)
         self.layoutVertical2.addWidget(self.canvas2)
 
+# map data
 class MatplotlibWidget(QtGui.QWidget):
     def __init__(self,parent=None):
         super(MatplotlibWidget,self).__init__(parent)
@@ -274,18 +361,26 @@ def make_some_data():
     total=total_targets+Boulder.noise_layer+Boulder.structured_noise_layer+Boulder.shotgun_noise_layer
     return total
 
+def get_intensity():
+    genus=np.random.randint(5)
+    print genus
+    genus=0
+    target=Cumuliform(genus=genus,weather=True)
+    return target.intensityModel
+
 def DirPrior():
     table=np.zeros((5,15,15))
-    base_table=np.array([[0.0817438692,0.1634877384,0.0136239782,0.0544959128,0.1634877384,0.0272479564,0.0272479564,0.0953678474,0.0544959128,0.0408719346,0.068119891,0.0544959128,0.0326975477,0.0544959128,0.068119891],
-            [0.0476190476,0.1428571429,0.0238095238,0.0714285714,0.1428571429,0.0119047619,0.0357142857,0.0833333333,0.0714285714,0.0476190476,0.119047619,0.0238095238,0.0238095238,0.0476190476,0.1071428571],
-            [0.047318612,0.1261829653,0.0630914826,0.0378548896,0.0630914826,0.094637224,0.1261829653,0.047318612,0.0157728707,0.0630914826,0.141955836,0.0315457413,0.0315457413,0.047318612,0.0630914826],
-            [0.0338983051,0.0847457627,0.0508474576,0.0508474576,0.2033898305,0.0338983051,0.0169491525,0.0338983051,0.1355932203,0.0508474576,0.1186440678,0.0169491525,0.0338983051,0.1016949153,0.0338983051],
-            [0.0282258065,0.0483870968,0.0967741935,0.0201612903,0.0483870968,0.1612903226,0.0403225806,0.060483871,0.060483871,0.0201612903,0.0403225806,0.1814516129,0.1008064516,0.0806451613,0.0120967742]])
+    #  base_table=np.array([[0.0817438692,0.1634877384,0.0136239782,0.0544959128,0.1634877384,0.0272479564,0.0272479564,0.0953678474,0.0544959128,0.0408719346,0.068119891,0.0544959128,0.0326975477,0.0544959128,0.068119891],
+    #          [0.0476190476,0.1428571429,0.0238095238,0.0714285714,0.1428571429,0.0119047619,0.0357142857,0.0833333333,0.0714285714,0.0476190476,0.119047619,0.0238095238,0.0238095238,0.0476190476,0.1071428571],
+    #          [0.047318612,0.1261829653,0.0630914826,0.0378548896,0.0630914826,0.094637224,0.1261829653,0.047318612,0.0157728707,0.0630914826,0.141955836,0.0315457413,0.0315457413,0.047318612,0.0630914826],
+    #          [0.0338983051,0.0847457627,0.0508474576,0.0508474576,0.2033898305,0.0338983051,0.0169491525,0.0338983051,0.1355932203,0.0508474576,0.1186440678,0.0169491525,0.0338983051,0.1016949153,0.0338983051],
+    #          [0.0282258065,0.0483870968,0.0967741935,0.0201612903,0.0483870968,0.1612903226,0.0403225806,0.060483871,0.060483871,0.0201612903,0.0403225806,0.1814516129,0.1008064516,0.0806451613,0.0120967742]])
+    base_table=np.ones((5,15))
     for i in range(15):
         table[:,:,i]=base_table
     table*=0.1
     for i in range(15):
-        table[:,i,i]*=10
+        table[:,i,i]*=3
     #  print table
     return table
 
@@ -293,6 +388,7 @@ def DirPrior():
 if __name__ == '__main__':
     total=make_some_data()
     prior=DirPrior()
+    intensity=get_intensity()
     app=QtGui.QApplication(sys.argv)
-    ex=Window(total,prior)
+    ex=Window(total,intensity,prior)
     sys.exit(app.exec_())
